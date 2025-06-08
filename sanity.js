d94abc406daa97599917cecf24769904982624ce
shahudahmed <shahudvivo@gmail.com>
2025-06-08 22:36:43 +0530

    Create sanity.js

---
+++ sanity.js
@@ -0,0 +1,11 @@
+// my-website/sanityClient.jsAdd commentMore actions
+
+import {createClient} from '@sanity/client'
+
+export const client = createClient({
+  // Find your project ID in your sanity.json file
+  projectId: 'x2dxeg98',
+  dataset: 'production',
+  useCdn: true, // set to `false` for fresh data
+  apiVersion: '2025-06-08', // use a current date
+})
